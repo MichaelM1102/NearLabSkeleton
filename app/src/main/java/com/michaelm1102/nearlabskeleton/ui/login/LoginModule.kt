62749f7969c082bd6fb69377688fac5b616466ea
michael <michaelboymek@gmail.com>
2022-02-09 23:04:23 +0300

    First_Commit

--- app/src/main/java/com/michaelm1102/nearlabskeleton/ui/login/LoginModule.kt
+++ app/src/main/java/com/michaelm1102/nearlabskeleton/ui/login/LoginModule.kt
@@ -1,19 +1,29 @@
-package com.michaelm1102.nearlabskeleton.data.network.login
+package com.michaelm1102.nearlabskeleton.ui.login
 
+import com.michaelm1102.nearlabskeleton.data.network.login.LoginApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
-import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 
 @Module
 @InstallIn(ViewModelComponent::class)
-object LoginApiModule {
+object LoginModule {
     @Provides
     @ViewModelScoped
     fun provideLoginApi(retrofit: Retrofit): LoginApi {
         return retrofit.create(LoginApi::class.java)
     }
+
+    // if dao exist provide login dao
+    // and embed in repository
+    @Provides
+    @ViewModelScoped
+    fun provideLoginRepository(loginApi: LoginApi): LoginRepository {
+        return LoginRepository(loginApi)
+    }
+
+
 }
---
+++ app/src/main/java/com/michaelm1102/nearlabskeleton/ui/login/LoginRepository.kt
@@ -0,0 +1,6 @@
+package com.michaelm1102.nearlabskeleton.ui.login
+
+import com.michaelm1102.nearlabskeleton.data.network.login.LoginApi
+
+class LoginRepository(private val loginService:LoginApi) {
+}
